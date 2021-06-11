d96af8f7543fdfb8479d0e5ac1823dccca313f4c
Tal Mikey <tal.mikey@gmail.com>
2021-06-11 20:00:02 +0300

    wip

---
+++ SportStore/Facebook/FacebookHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace SportStore.Facebook
+{
+    public class FacebookHelper
+    {
+        public WebResponse PostOnFacebook(string message)
+        {
+            const string accessToken = "accessToken";
+            const string appId = "appId";
+
+            var url = $"https://graph.facebook.com/{appId}/feed?message={message}&access_token={accessToken}";
+
+            var httpRequest = WebRequest.Create(url);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/x-www-form-urlencoded";
+
+            return httpRequest.GetResponse();
+        }
+    }
+}
