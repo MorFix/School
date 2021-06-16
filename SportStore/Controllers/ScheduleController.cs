aa0ab5274ccc82a61dcdca66e49d93ddbe9587b3
omerdital <omerdital@gmail.com>
2021-06-16 17:36:59 +0300

    feat: schedule page

---
+++ SportStore/Controllers/ScheduleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.DataBase;
+using SportStore.Entities;
+using System.Collections.Generic;
+
+namespace SportStore.Controllers
+{
+    [Route("schedule")]
+    public class ScheduleController : Controller
+    {
+        private SchoolContext _ctx;
+
+        public ScheduleController(SchoolContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new List<Lesson> { 
+                new() {Subject="english", DayOfWeek=System.DayOfWeek.Monday, Hour = 4},
+                new() {Subject="hebrew", DayOfWeek=System.DayOfWeek.Tuesday, Hour = 4},
+                new() {Subject="ccc", DayOfWeek=System.DayOfWeek.Wednesday, Hour = 4},
+                new() {Subject="ddddd", DayOfWeek=System.DayOfWeek.Wednesday, Hour = 4},
+                new() {Subject="eeeeee", DayOfWeek=System.DayOfWeek.Wednesday, Hour = 4},
+                new() {Subject="fffff", DayOfWeek=System.DayOfWeek.Wednesday, Hour = 4},
+            });
+        }
+    }
+}
---
+++ SportStore/Logic/ScheduleLogic.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportStore.Entities;
+
+namespace SportStore.Logic
+{
+    public class ScheduleLogic : BaseLogic<Class>
+    {
+        public override DbSet<Class> GetEntities()
+        {
+            return Context.Classes;
+        }
+    }
+}
