09d95adede6b6513b893267b8afddd9a1c887644
omerdital <omerdital@gmail.com>
2021-08-29 18:01:34 +0300

    add lesson crud

---
+++ SportStore/Controllers/.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportStore.DataBase;
+using SportStore.Entities;
+
+namespace SportStore.Controllers
+{
+    public class LessonController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public LessonController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lesson
+        public async Task<IActionResult> Index()
+        {
+            var schoolContext = _context.Lessons.Include(l => l.Room).Include(l => l.Teacher);
+            return View(await schoolContext.ToListAsync());
+        }
+
+        // GET: Lesson/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .Include(l => l.Room)
+                .Include(l => l.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // GET: Lesson/Create
+        public IActionResult Create()
+        {
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name");
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName");
+
+            return View();
+        }
+
+        // POST: Lesson/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeacherId,RoomId,Subject,DayOfWeek,Hour,Id")] Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                lesson.Id = Guid.NewGuid();
+                _context.Add(lesson);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Id", lesson.RoomId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", lesson.TeacherId);
+            return View(lesson);
+        }
+
+        // GET: Lesson/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name", lesson.RoomId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", lesson.TeacherId);
+            return View(lesson);
+        }
+
+        // POST: Lesson/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("TeacherId,RoomId,Subject,DayOfWeek,Hour,Id")] Lesson lesson)
+        {
+            if (id != lesson.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lesson);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LessonExists(lesson.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name", lesson.RoomId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", lesson.TeacherId);
+            return View(lesson);
+        }
+
+        // GET: Lesson/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .Include(l => l.Room)
+                .Include(l => l.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // POST: Lesson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var lesson = await _context.Lessons.FindAsync(id);
+            _context.Lessons.Remove(lesson);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LessonExists(Guid id)
+        {
+            return _context.Lessons.Any(e => e.Id == id);
+        }
+    }
+}
