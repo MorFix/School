9f56ffbf43bfa277b33a3999d172133fd68ec766
omerdital <omerdital@gmail.com>
2021-09-01 18:29:36 +0300

    feat: add graphs

--- School/Controllers/StudentsController.cs
+++ School/Controllers/StudentsController.cs
@@ -6,6 +6,8 @@ using School.Controllers.Security;
 using School.Enums;
 using System.Threading.Tasks;
 using System;
+using System.Data.Entity;
+using School.ViewModels;
 
 namespace School.Controllers
 {
@@ -25,8 +27,42 @@ namespace School.Controllers
         {
             var students = _ctx.Students.ToList();
             ViewData["Behavior"] = StudentBehavior.Good.ToSelectList();
+            ViewBag.StudentsByBehavior = students.GroupBy(x => x.Behavior)
+                .ToDictionary(x => x.Key, x => x.Count());
               
             return View(students);
+        }
+
+        [Permissions(PermissionsLevel.Manage)]
+        [HttpGet]
+        [Route("studentsByBehavior")]
+        public IActionResult StudentsByBehavior()
+        {
+            var studentsByBehavior = _ctx.Students.ToList().GroupBy(x => x.Behavior)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            return Ok(studentsByBehavior);
+        }
+
+        [Permissions(PermissionsLevel.Manage)]
+        [HttpGet]
+        [Route("studentsByClass")]
+        public IActionResult StudentsByClasses()
+        {
+            var studentsCountByClass = _ctx.Classes
+                .Join(
+                    _ctx.Students,
+                    c => c.Id,
+                    s => s.ClassId,
+                    (c, s) => new
+                    {
+                        ClassName = c.Name,
+                    }
+                ).ToList().OrderBy(x => x.ClassName)
+                .GroupBy(x => x.ClassName)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            return Ok(studentsCountByClass);
         }
 
         [Permissions(PermissionsLevel.Manage)]
